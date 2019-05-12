b767de54dafec83f4feaece7b12a5f5d69c04200
Dustin Scott <dustin001@live.missouristate.edu>
2019-05-11 22:37:14 -0500

    fixed misplaced code

--- fireDetection.py
+++ fireDetection.py
@@ -14,25 +14,36 @@ low_confidence = False
 fire_cascade = cv2.CascadeClassifier('myhaar.xml')
 video_stream = cv2.VideoCapture('kitchenfire1.mp4')
 last_analysis_time = time.time()
+noFireDetectedCount = 0
+
 
 def analyzeDetectionRate():
-    global last_analysis_time, fire_frames_detected, old_frames_detected_value, high_confidence, moderate_confidence, low_confidence
+    global last_analysis_time, fire_frames_detected, old_frames_detected_value, high_confidence, moderate_confidence, low_confidence, noFireDetectedCount
 
-    if ((time.time() - last_analysis_time) > 1.0):
-        if fire_frames_detected - old_frames_detected_value > 6:
+    #Analyze detection counts every 2 seconds:
+    if ((time.time() - last_analysis_time) > 2.0):
+        if fire_frames_detected - old_frames_detected_value > 24:
             high_confidence = True
             moderate_confidence = False
             low_confidence = False
-        elif fire_frames_detected - old_frames_detected_value > 4:
+        elif fire_frames_detected - old_frames_detected_value > 12:
             high_confidence = False
             moderate_confidence = True
             low_confidence = False
-        elif fire_frames_detected - old_frames_detected_value > 2:
+        elif fire_frames_detected - old_frames_detected_value > 1:
             high_confidence = False
             moderate_confidence = False
             low_confidence = True
+        else:
+            #If no fire frames are detected after several repetitions, clear the warning:
+            noFireDetectedCount += 1
+            if noFireDetectedCount > 2:
+                high_confidence = False
+                moderate_confidence = False
+                low_confidence = False
+                noFireDetectedCount = 0
         old_frames_detected_value = fire_frames_detected
-    last_analysis_time = time.time()
+        last_analysis_time = time.time()
 
 
 def main():
@@ -41,6 +52,7 @@ def main():
         ret, img = video_stream.read()
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         global fire_frames_detected
+        analyzeDetectionRate()
 
         # last two parameters are detection weights:
         # first paramater is threshold for positive detection: lower means more likely to detect
@@ -48,16 +60,16 @@ def main():
         fire = fire_cascade.detectMultiScale(gray, 1.3, 13)
             
         for (x,y,w,h) in fire:
-            cv2.rectangle(img,(x,y),(x+w,y+h),(0,0,255),2)
+            #Create a rectangle around the detected area:
+            cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), 2)
             fire_frames_detected += 1
-            analyzeDetectionRate()
 
-            if high_confidence:
-                cv2.putText(img, "HIGH CONFIDENCE FIRE", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (255, 255, 255), lineType=cv2.LINE_AA)
-            elif moderate_confidence:
-                cv2.putText(img, "MODERATE CONFIDENCE FIRE", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (255, 255, 255), lineType=cv2.LINE_AA)
-            elif low_confidence:
-                cv2.putText(img, "LOW CONFIDENCE FIRE", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (255, 255, 255), lineType=cv2.LINE_AA)
+        if high_confidence:
+            cv2.putText(img, "HIGH CONFIDENCE FIRE", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (76, 255, 255), 2, lineType=cv2.LINE_AA)
+        elif moderate_confidence:
+            cv2.putText(img, "MODERATE CONFIDENCE FIRE", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (76, 255, 255), 2, lineType=cv2.LINE_AA)
+        elif low_confidence:
+            cv2.putText(img, "LOW CONFIDENCE FIRE", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (76, 255, 255), 2, lineType=cv2.LINE_AA)
 
         cv2.imshow('img',img)
         k = cv2.waitKey(30) & 0xff
